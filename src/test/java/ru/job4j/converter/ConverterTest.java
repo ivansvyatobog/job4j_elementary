ae521753a1986053fc7d596f866141ba66d2a1a3
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-07 01:23:38 +0300

    Модульные тесты

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -6,4 +6,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ConverterTest {
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert140RblThen2dot3333Dlr() {
+        float in = 140;
+        float expected = 2.3333333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
 }
