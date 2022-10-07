e61f9f0e384e8d9bd6dd789cb049b8b8322c4a1e
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-07 18:59:51 +0300

    Правка

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -1,11 +1,4 @@
 package ru.job4j.condition;
 
 public class Greeting {
-    public static void main(String[] args) {
-        String idea = "I like Java! ";
-        idea += "But I am a newbie. ";
-        int year = 2022;
-        idea += year;
-        System.out.println(idea);
-    }
 }
