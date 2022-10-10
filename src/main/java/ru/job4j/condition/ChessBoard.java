b84bbba85a3e3c431f497a134e8c158e4be427ca
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-11 01:07:53 +0300

    Math.abs

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -7,7 +7,7 @@ public class ChessBoard {
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
             if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
-                rsl = rsl == 0 ? Math.abs(y1 - y2) : rsl;
+                rsl = Math.abs(x1 - x2);
             }
         }
         return rsl;
