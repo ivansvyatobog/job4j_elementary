166336d93671d87945d51fd3074b49e51acf62c2
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-12 20:10:10 +0300

    Заполнение массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(age.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Sergey Tihonov";
+        names[2] = "Ivan Svyatobog";
+        names[3] = "Vitaliy Petrenko";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
