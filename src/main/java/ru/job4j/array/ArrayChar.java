d430c86dc728481eb9631e9a634d7849261c9f9a
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-14 15:55:30 +0300

    правка

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,16 +2,4 @@ package ru.job4j.array;
 
 public class ArrayChar {
 
-    public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = true;
-        for (int i = 0; i < pref.length; i++) {
-            for (int j = 0; j < word.length; j++) {
-                if (pref[i] != word[i]) {
-                    result = false;
-                    break;
-                }
-            }
-        }
-        return result;
-    }
 }
--- src/test/java/ru/job4j/array/ArrayCharTest.java
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -4,35 +4,5 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArrayCharTest {
-    @Test
-    public void whenStartWithPrefixThenTrue() {
-        char[] word = {'H', 'e', 'l', 'l', 'o'};
-        char[] pref = {'H', 'e'};
-        boolean result = ArrayChar.startsWith(word, pref);
-        assertThat(result).isTrue();
-    }
 
-    @Test
-    public void whenNotStartWithPrefixThenFalse() {
-        char[] word = {'H', 'e', 'l', 'l', 'o'};
-        char[] pref = {'H', 'i'};
-        boolean result = ArrayChar.startsWith(word, pref);
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    public void whenNotStartWithPrefixThenFalse2() {
-        char[] word = {'W', 'r', 'o', 'n', 'g'};
-        char[] pref = {'H', 'i'};
-        boolean result = ArrayChar.startsWith(word, pref);
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    public void whenStartWithPrefixThenTrue2() {
-        char[] word = {'R', 'i', 'g', 'h', 'i'};
-        char[] pref = {'R', 'i'};
-        boolean result = ArrayChar.startsWith(word, pref);
-        assertThat(result).isTrue();
-    }
 }
