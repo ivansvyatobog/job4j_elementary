7d3198210c838e41ff2df8c74906caa302ed7c27
ivansvyatobog <kelthasaugust@yandex.ru>
2022-10-24 20:25:34 +0300

    Перегрузить метод max для трех чисел

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,27 +6,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.max(first, second);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenMax9To4Then9() {
-        int left = 9;
-        int right = 4;
-        int result = Max.max(left, right);
+        int first = 9;
+        int second = 4;
+        int result = Max.max(first, second);
         int expected = 9;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenEqualsThen7() {
-        int left = 7;
-        int right = 7;
-        int result = Max.max(left, right);
+        int first = 7;
+        int second = 7;
+        int result = Max.max(first, second);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void maxFrom3Parameters() {
+        int first = 9;
+        int second = 1;
+        int third = 15;
+        int result = Max.max(first, second, third);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void maxFrom4Parameters() {
+        int first = 7;
+        int second = 7;
+        int third = 5;
+        int fourth = 1;
+        int result = Max.max(first, second, third, fourth);
         int expected = 7;
         assertThat(result).isEqualTo(expected);
     }
